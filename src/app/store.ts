8b239d147e94421de5780537e9ea10c34b5a8bc1
sk <jsckhan@outlook.com>
2023-01-09 22:21:04 -0500

    fix: StateInvariant Middleware Crashing Redux
    Disabled development middleware checking for state invariance,
    (time to execute exceeded 32ms per action). Large number of pokeApi
    calls for the application are made at once.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,14 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import pokeApi from "../services/pokeApi";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		[pokeApi.reducerPath]: pokeApi.reducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}).concat(pokeApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
