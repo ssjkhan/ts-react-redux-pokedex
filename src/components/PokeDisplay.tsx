9c068861483cefffb752486c8b6d46ceefe1d3fc
sk <jsckhan@outlook.com>
2023-01-10 14:50:25 -0500

    feat: Display Cards for pokemon
    Added pokemon display cards for each pokemon with elipses for links
    to detailed views.

--- src/components/PokeDisplay.tsx
+++ src/components/PokeDisplay.tsx
@@ -2,51 +2,71 @@ import React from "react";
 import { useGetPokemonByNumberQuery } from "../services/pokeApi";
 import CSS from "csstype";
 import PokeStats from "./PokeStats";
+import PokeLogo from "./PokeLogo";
+import "./pokeDisplay.css"
 
 interface Props {
 	number: number;
+	underCon: Function;
 }
 
 function PokeDisplay(props: Props) {
 	const { data, error, isLoading } = useGetPokemonByNumberQuery(props.number);
 
-	const imgStyle: CSS.Properties = {
-		WebkitFilter: "grayscale(100%)",
-		filter: "grayscale(100%)",
+	const imgStyle1: CSS.Properties = {
+		// WebkitFilter: "grayscale(100%)",
+		// filter: "grayscale(100%)",
 		height: "10rem",
 		width: "10rem",
 	};
+
 	const cardStyle: CSS.Properties = {
 		borderLeft: "0.3rem solid var(--bs-yellow)",
 		borderBottom: "0.25rem solid var(--bs-dark)",
 		boxShadow: "0.25rem",
 	};
 
+	
+	
+
 	return (
 		<>
 			<div className="col-xl-3 col-md-6 mb-4">
 				<div
 					className="card border-left-darkborder-top- shadow h-100 py-2 mw-100 mh-100 "
-					style={cardStyle}
+					
 				>
 					<div className="card-body">
-						<div className="row no-gutters align-items-center">
-							<div id="pokePicture col-6">
+						<div className="row no-gutters d-flex align-items-center justify-content-center">
+							<div
+								id="pokePicture"
+								className="col-6 d-flex align-items-center justify-content-center"
+							>
 								{error ? (
 									<>Oh no, there was an error</>
 								) : isLoading ? (
-									<>Loading...</>
+									<PokeLogo />
 								) : data ? (
 									<>
 										<img
-											style={imgStyle}
+											className="pokeImg pokeCard"
 											src={data.sprites.front_default}
-											alt={data.species.name}
 										/>
 									</>
 								) : null}
 							</div>
+						</div>
+
+						<div className="row no-gutters d-flex align-items-center justify-content-center">
 							<PokeStats />
+						</div>
+						<div className="row no-gutters d-flex justify-content-end">
+							<i
+								className="fa-solid fa-ellipsis w-auto "
+								onClick={() => {
+									props.underCon();
+								}}
+							></i>
 						</div>
 					</div>
 				</div>
