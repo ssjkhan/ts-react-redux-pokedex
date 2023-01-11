4a33fdeb1ee178d5e48cd672dfcc829d0bf2c044
sk <jsckhan@outlook.com>
2023-01-11 15:50:28 -0500

    feat: PokeStat components reflect stat values
    Static PokeStat bars are now replaced with stat bars that reflect the
    pokemon's value for the current stat as returned from the api.

--- src/components/PokeStat.tsx
+++ src/components/PokeStat.tsx
@@ -1,23 +1,67 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { ProgressBar } from "react-bootstrap";
 import "./pokeStat.css";
 
 function PokeStat(props: any) {
-	const [isGray, setGray] = useState(true);
+  const [isGray, setGray] = useState(true);
 
-	useEffect(() => {
-		props.onMount(setGray);
-	});
+  const mapStatName: (name: string) => string = (name) => {
+    switch (name) {
+      case "hp":
+        return "HP";
+        break;
+      case "attack":
+        return "ATT";
+        break;
+      case "defense":
+        return "DEF";
+        break;
+      case "special-attack":
+        return "SATT";
+        break;
+      case "special-defense":
+        return "SDEF";
+        break;
+      case "speed":
+        return "SPD";
+        break;
+      default:
+        return "";
+    }
+  };
 
-	return (
-		<div className="row d-flex flex-row justify-content-between">
-			<div className="col-3">{props.name}</div>
-			<div
-				className={isGray ? "col-6 bg-gray my-1" : "col-6 bg-primary my-1"}
-			></div>
+  const barClass: () => string = () => {
+    var statVal: number = Math.floor(Number(props.stat.base_stat) / 120) * 100;
+    var className: string = `w-${statVal} mx-0 px-0`;
+    if (isGray) {
+      className += " bg-gray";
+    } else {
+      className += " bg-primary";
+    }
+    return className;
+  };
+  const statVal = Math.floor(Number(props.stat.base_stat) / 125 * 100);
 
-			<div className="col-3 text-end">{props.val}</div>
-		</div>
-	);
+  useEffect(() => {
+    props.onMount(setGray);
+  });
+
+  return (
+    <div className="row d-flex flex-row justify-content-between">
+      <div className="col-xl-3 col-2 text-capitalize mx-0 px-0">
+        {mapStatName(props.stat.stat.name)}
+      </div>
+      <div className="col-xl-3 col-8 mx-0 px-0 my-1">
+        <ProgressBar
+          now={statVal}
+        />
+      </div>
+
+      <div className="col-xl-3 col-2 text-end mx-0 px-0">
+        {props.stat.base_stat}
+      </div>
+    </div>
+  );
 }
 
 export default PokeStat;
--- src/components/PokeStats.tsx
+++ src/components/PokeStats.tsx
@@ -2,14 +2,17 @@ import * as React from "react";
 import PokeStat from "./PokeStat";
 
 function PokeStats(props: any) {
-	return (
-		<>
-			<div className="container">
-				<PokeStat {...{ name: "Stat", val: 55, onMount: props.onMount }} />
-				<PokeStat {...{ name: "Stat", val: 55, onMount: props.onMount }} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container">
+        {props.stats.map((stat: any) => (
+          <PokeStat
+            {...{ name: "Stat", val: 55, onMount: props.onMount, stat: stat }}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default PokeStats;
