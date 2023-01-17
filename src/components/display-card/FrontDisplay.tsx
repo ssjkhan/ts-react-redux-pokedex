6080e24bde6ed404e25b389b3bda9442e7c29853
sk <jsckhan@outlook.com>
2023-01-17 12:39:04 -0500

    feat: Highlighinting of accent elements
    Add highlighting of accent elements on card hover.

--- src/components/display-card/FrontDisplay.tsx
+++ src/components/display-card/FrontDisplay.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import PokeLogo from "../PokeLogo";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import PokeSprite from "../sprite/PokeSprite";
 import PokeStats from "../stat/PokeStats";
 
 function FrontDisplay(props: any) {
+  const [isGray, setGray] = useState(false);
+  const targetRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    props.onMountStats(setGray);
+  });
+
+  const iconClassNameGray = "fa-solid fa-bars";
+  const iconClassNameColor = "text-info fa-solid fa-bars";
+
   return (
     <>
       <div className="row no-gutters d-flex align-items-center justify-content-center">
@@ -14,16 +22,16 @@ function FrontDisplay(props: any) {
           <PokeSprite
             isError={props.isError}
             isLoading={props.isLoading}
-            sprite={props.sprites.front_default}
+            sprite={props.pokemonData.sprites.front_default}
             onMount={props.onMountPicture}
           />
         </div>
       </div>
       <div className="row no-gutters d-flex align-items-center justify-content-center">
         <PokeStats
-          number={props.number}
+          number={props.pokeID}
           onMount={props.onMountStats}
-          stats={props.stats}
+          stats={props.pokemonData.stats}
         />
       </div>
       <div className="row no-gutters d-flex align-items-bottom justify-content-center mt-3 ">
@@ -35,7 +43,7 @@ function FrontDisplay(props: any) {
             props.onClickCard(e);
           }}
         >
-          <i className="fa-solid fa-bars">
+          <i className={isGray ? iconClassNameGray : iconClassNameColor}>
           </i>
         </div>
       </div>
