aa18cd2e05788d5648c291bb94d42a7a80fab68f
sk <jsckhan@outlook.com>
2023-01-17 12:38:19 -0500

    feat: Update Data calls to parent
    API calls are made in parent to avoid complex callbacks and state
    management for components.

--- src/components/display-card/PokeDisplay.tsx
+++ src/components/display-card/PokeDisplay.tsx
@@ -1,5 +1,8 @@
 import React, { useRef, useState } from "react";
-import { useGetPokemonByNumberQuery } from "../../services/pokeApi";
+import {
+  useGetPokemonByNumberQuery,
+  useGetSpeciesByNumberQuery,
+} from "../../services/pokeApi";
 import PokeStats from "../stat/PokeStats";
 import PokeLogo from "../PokeLogo";
 import PokeSprite from "../sprite/PokeSprite";
@@ -12,8 +15,49 @@ interface Props {
 }
 
 function PokeDisplay(props: Props) {
-  const { data, error, isLoading } = useGetPokemonByNumberQuery(props.number);
+  const {
+    data: pokemonData,
+    error: getPokemonError,
+    isLoading: pokemonIsLoading,
+  } = useGetPokemonByNumberQuery(props.number);
+  const {
+    data: speciesData,
+    error: getSpeciesError,
+    isLoading: speciesIsLoading,
+  } = useGetSpeciesByNumberQuery(props.number);
   const [isFront, setFront] = useState(true);
+  const [isGray, setGray] = useState(true);
+
+  const classNameCardGray =
+    "card border-left-dark  shadow h-100 py-2 mw-100 mh-100  ";
+
+  const typeColorDict: any = {
+    "normal": "warning",
+    "water": "primary",
+    "fire": "danger",
+    "electric": "warning",
+    "grass": "success",
+    "ice": "info",
+    "fighting": "danger",
+    "poison": "dark",
+    "ground": "warning",
+    "flying": "seconday",
+    "psychic": "info",
+    "bug": "success",
+    "ghost": "dark",
+    "dragon": "primary",
+    "dark": "dark",
+    "steel": "secondary",
+    "fairy": "danger",
+  };
+  const classNameCardColor = () => {
+    if (!pokemonData) {
+      return "card border border-dark border-5 shadow h-100 py-2 mw-100 mh-100 ";
+    }
+    var color = typeColorDict[pokemonData.types[0].type.name];
+    return `card border border-${color} border-5 shadow h-100 py-2 mw-100 mh-100`;
+  };
+
   // Get handles on bar and image isHover state
   let grayStatsArr: Function[] = [];
   let updateGrayStats = (val: boolean) => {
@@ -36,6 +80,7 @@ function PokeDisplay(props: Props) {
   const changeGrayScale = (val: boolean) => {
     updateGrayPicture(val);
     updateGrayStats(val);
+    setGray(val);
   };
 
   const onMouseEnter = () => {
@@ -44,7 +89,7 @@ function PokeDisplay(props: Props) {
 
   const onMouseExit = () => {
     changeGrayScale(true);
-    // setFront(true);
+    setFront(true);
   };
 
   const myRef = useRef<HTMLDivElement>(null);
@@ -67,41 +112,42 @@ function PokeDisplay(props: Props) {
         onMouseEnter={() => onMouseEnter()}
         onMouseLeave={() => onMouseExit()}
       >
-        <div className="card border-left-dark border-top- shadow h-100 py-2 mw-100 mh-100 ">
+        <div className={isGray ? classNameCardGray : classNameCardColor()}>
           <div className="card-body">
             <div className="row no-gutters d-flex align-items-center justify-content-center">
-              {error
+              {getPokemonError
                 ? <>Oh no, there was an error</>
-                : isLoading
+                : pokemonIsLoading
                 ? <PokeLogo />
-                : data
+                : pokemonData
                 ? isFront
                   ? (
                     <FrontDisplay
+                      pokeID={props.number}
+                      pokemonData={pokemonData}
+                      isError={getPokemonError}
+                      isLoading={pokemonIsLoading}
+                      onClickCard={onClickCard}
                       conAlert={props.underCon}
                       setFront={setFront}
                       onMountStats={onMountStats}
-                      stats={data.stats}
                       onMountPicture={onMountPicture}
-                      isError={error}
-                      isLoading={isLoading}
-                      onClickCard={onClickCard}
-                      sprites={data.sprites}
                     />
                   )
                   : (
                     <BackDisplay
                       pokeID={props.number}
+                      pokemonData={pokemonData}
+                      speciesData={speciesData}
+                      isLoading={pokemonIsLoading && speciesIsLoading}
+                      isError={getPokemonError && getSpeciesError}
+                      Error={{ getPokemonError, getSpeciesError }}
+                      stats={pokemonData.stats}
                       conAlert={props.underCon}
                       setFront={setFront}
-                      onMountStats={onMountStats}
-                      stats={data.stats}
                       onMountPicture={onMountPicture}
-                      isError={error}
-                      isLoading={isLoading}
+                      onMountStats={onMountStats}
                       onClickCard={onClickCard}
-                      sprites={data.sprites}
-                      data={data}
                     />
                   )
                 : <></>}
