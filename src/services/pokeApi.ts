7ca86c9ff6321449eecc29add08555a49af1be80
sk <jsckhan@outlook.com>
2023-01-16 20:40:17 -0500

    feat: Add RTK endpoint for species data

--- src/services/pokeApi.ts
+++ src/services/pokeApi.ts
@@ -1,34 +1,38 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface Pokemon {
-	species: {
-		name: string;
-	};
-	sprites: { front_shiny: any };
+  species: {
+    name: string;
+  };
+  sprites: { front_shiny: any };
 }
 
 export const pokeApi = createApi({
-	reducerPath: "pokeApi",
-	baseQuery: fetchBaseQuery({
-		baseUrl: "https://pokeapi.co/api/v2/",
-	}),
-	endpoints: (builder) => ({
-		testEndPoint: builder.query<any, string>({
-			query: () => `pokemon/bulbasaur`,
-		}),
-		getPokemonByName: builder.query<any, string>({
-			query: (name) => `pokemon/${name}`,
-		}),
-		getPokemonByNumber: builder.query<any, number>({
-			query: (number) => `pokemon/${number}`,
-		}),
-	}),
+  reducerPath: "pokeApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://pokeapi.co/api/v2/",
+  }),
+  endpoints: (builder) => ({
+    testEndPoint: builder.query<any, string>({
+      query: () => `pokemon/bulbasaur`,
+    }),
+    getPokemonByName: builder.query<any, string>({
+      query: (name) => `pokemon/${name}`,
+    }),
+    getPokemonByNumber: builder.query<any, number>({
+      query: (number) => `pokemon/${number}`,
+    }),
+    getSpeciesByNumber: builder.query<any, number>({
+      query: (number) => `pokemon-species/${number}`,
+    }),
+  }),
 });
 
 export const {
-	useGetPokemonByNameQuery,
-	useGetPokemonByNumberQuery,
-	useTestEndPointQuery,
+  useGetPokemonByNameQuery,
+  useGetPokemonByNumberQuery,
+  useTestEndPointQuery,
+  useGetSpeciesByNumberQuery,
 } = pokeApi;
 
 export default pokeApi;
